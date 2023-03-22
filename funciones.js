871b627d4004d47b1eb24f229481331dd2595ec9
luz López <lalopez6907@misena.edu.co>
2023-03-22 12:18:59 -0500

    primer commit

---
+++ funciones.js
@@ -0,0 +1,16 @@
+//comentario de una línea
+/* comentario 
+     varias
+     lineas
+*/
+// no es necesario declarar variables antes de utilizarla
+// una variable es un lugaren memoria a la cual le asigno un valor para ser usada
+ 
+// variavle de ambito de función
+var a;
+
+// variable de ambito de bloque
+let b;
+
+//operaciones:
+// estructuras de datos : arreglos ...
